111d4c28b6b6a09c9f16588197fc10b526e31074
IdikaEliada <idika1996@gmail.com>
2025-10-10 10:48:43 +0100

    update

---
+++ script/components/overview.js
@@ -0,0 +1,18 @@
+export default function(target){
+  target.innerHTML = `      <div class="py-6">
+            The Student Union Government (SUG) of the Federal University of Technology, Owerri (FUTO) is the official student-run body dedicated to representing the interests, rights, and welfare of over 30,000 Futoites.
+            
+            <br /><br />
+            Established in [insert founding year, e.g., 1981 when FUTO was founded], SUG has been the heartbeat of campus activism, bridging the gap between students and the university administration. 
+            
+            <br />
+            <br />From advocating for better hostel conditions to organizing skill-building workshops, we are committed to fostering a vibrant, inclusive, and progressive academic community. As of October 2025, under the leadership of the newly elected 2025/2026 executive team, we continue to uphold the spirit of 'Aluta Continua, Victoria Acerta'—the struggle continues, and victory is certain.
+          </div>
+          
+      <div class="md:hidden flex bg-black overflow-hidden rounded position-relative min-w-[300px] md:w-[750px]" data-aos="zoom-in" data-aos-delay="200">
+        <img src="https://i.ibb.co/N6kkcH7y/20251009-215749.jpg" class="main-image swiper-slide" loading="lazy" 
+        />
+            <!--img src="https://i.ibb.co/bjSNLbZK/20251010-020850.jpg" class="main-image swiper-slide" loading="lazy" 
+            /-->
+      </div>`
+}
