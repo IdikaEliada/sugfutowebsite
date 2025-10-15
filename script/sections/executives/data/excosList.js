02cce094ed09d9c30db1d100c9a1573373fea16f
IdikaEliada <idika1996@gmail.com>
2025-10-15 21:15:51 +0100

    update excosList.js

--- script/sections/executives/data/excosList.js
+++ script/sections/executives/data/excosList.js
@@ -5,13 +5,149 @@ export const excosList = [{
   id: 27,
   about: "The 27th Elected SUG Executives for FUTO",
   logo: "https://i.ibb.co/WWmX9221/20251012-173114.png",
-  members: null,
+  color: 'blue',
+  members: [{
+    name: "Christopher Bukas",
+    position: "President",
+    about: "Leading with vision, excellence, and innovation — ",
+    role: "Dedicated to progress, unity, and effective leadership, representing the voice and interest of all students as the President of the Aluta Solution Executives.",
+    image: null,
+    titles: "HE. Bar. Hon. High Comr",
+    id: {
+      group: "SUGCORE",
+      number: 1
+    }
+  }, {
+    name: "Echeonwu Chidera",
+    position: "Vice President",
+    about: "A passionate advocate for students welfare, bringing vision, energy, and a collaborative spirit to the executive council. ",
+    role: "Her leadership is focused on inclusion, unity, and growth.",
+    image: null,
+    titles: "HE. High Comr.",
+    id: {
+      group: "SUGCORE",
+      number: 2
+    }
+  }, {
+    name: "Mark Christopher Ikechukwu",
+    position: "Secretary General",
+    about: "Diligent, organized, and committed to transparency, ensuring smooth communication and documentation within the association.",
+    role: "He is hardworking and thorough, consistently putting in the effort to complete tasks accurately and on time.",
+    image: null,
+    titles: "Hon. Barr. High Comr.",
+    id: {
+      group: "SUGCORE",
+      number: 3
+    }
+  }, {
+    name: "Nduka Victory",
+    position: "Director of Information",
+    about: "Passionate about driving innovation and research, ensuring the community stays ahead on current happenings and relevant information",
+    role: "",
+    image: null,
+    titles: "Hon. Journ. High Comr.",
+    id: {
+      group: "SUGCORE",
+      number: 4
+    }
+  }, {
+    name: "Isaac Johnbosco",
+    position: "Director of Welfare",
+    about: "",
+    role: "Dedicated to ensuring the well-being of the community, making sure every student feels supported and valued.",
+    image: null,
+    titles: "High Comr.",
+    id: {
+      group: "SUGCORE",
+      number: 5
+    }
+  }, {
+    name: "Iheme Daniel",
+    position: "Director of Sports",
+    about: "",
+    role: "Passionate about bringing energy and teamwork to the community through exciting sports initiatives.",
+    image: null,
+    titles: "High Comr.",
+    id: {
+      group: "SUGCORE",
+      number: 6
+    }
+  }, {
+    name: "Ibim Great Graham",
+    position: "Director of Socials",
+    about: "",
+    role: "The creative mind behind social events and student engagement, ensuring fun meets purpose in every gathering.",
+    image: null,
+    titles: "High Comr.",
+    id: {
+      group: "SUGCORE",
+      number: 7
+    }
+  }, {
+    name: "Ugochukwu Chimakpa",
+    position: "Director of Transport",
+    about: "Staying in the loop and keeping everyone connected!",
+    role: "Dedicated to keeping the community informed and connected with the latest updates and opportunities.",
+    image: null,
+    titles: "Hon. High Comr.",
+    id: {
+      group: "SUGCORE",
+      number: 8
+    }
+  }, {
+    name: "Onubogu Chidera E.",
+    position: "Financial Secretary",
+    about: "",
+    role: "Committed to managing funds with precision and supporting the community's growth.",
+    image: null,
+    titles: "High Comr.",
+    id: {
+      group: "SUGCORE",
+      number: 9
+    }
+  }, {
+    name: "Obi Jennifer Chisom",
+    position: "Treasurer",
+    about: "",
+    role: "Committed to managing finances with care and transparency, ensuring the community thrives financially.",
+    image: null,
+    titles: "High Comr.",
+    id: {
+      group: "SUGCORE",
+      number: 10
+    }
+  }, {
+    
+    name: "Okafor Chioma Faithful",
+    position: "Assistant Secretary General",
+    about: "",
+    role: " ",
+    image: null,
+    titles: "High Comr.",
+    id: {
+      group: "SUGCORE",
+      number: 11
+    }
+  }, {
+    name: "",
+    position: "Director of Health ",
+    about: "",
+    role: "",
+    image: null,
+    titles: "High Comr.",
+    id: {
+      group: "SUGCORE",
+      number: 12
+    }
+  }
+  ],
   events: setEvent(27)
  },{
   name: "Aluta Transformation",
   id: 26,
   about: "The 26th Elected SUG Executives for FUTO",
   logo: "https://i.ibb.co/xKKvmFxb/20251011-083515.png",
+  color: 'blue',
   members: [{
     name: "Ogbonna Blessed",
     position: "President",
