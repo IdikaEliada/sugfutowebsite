0cd68ee60475ab7ce57cd356414a5325a515d12f
IdikaEliada <idika1996@gmail.com>
2025-11-01 19:07:11 +0100

    update

--- data/places_in_futo.js
+++ data/places_in_futo.js
@@ -11,49 +11,49 @@ export const allPlacesInFuto = [
     description: "Academic facility housing the Department of Environmental Health Sciences and Radiography programs with modern teaching facilities",
     address: "Federal University of Technology Owerri, PMB 1526, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+Environmental+Health+Sciences+Building",
-    images: ["image2_env_health_radiography.jpg"]
+    images: ["https://i.ibb.co/Tq8Kv9W1/pm-1754255252014-cmp.jpg"]
   },
   {
     name: "Garden of Unison",
-    description: "Central gathering space and outdoor venue used for student activities, relaxation, and community events under large shade trees",
+    description: "Central gathering space and outdoor venue used for student activities, relaxation, and community events under large shade trees, also called Rotract garden",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+Garden+of+Unison",
-    images: ["image3_garden_of_unison.jpg"]
+    images: ["https://i.ibb.co/ntScm9g/pm-1754255252407-cmp.jpg"]
   },
   {
     name: "Picnic Ground",
     description: "Shaded outdoor recreational area with mature trees, popular spot for student relaxation and informal gatherings",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+Picnic+Ground",
-    images: ["image4_picnic_ground.jpg"]
+    images: ["https://i.ibb.co/DDhqPFmd/pm-1754255250951-cmp.jpg"]
   },
   {
     name: "FUTO Cafe",
     description: "Main campus cafeteria and dining facility serving students and staff with various food options",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+Cafe+Owerri",
-    images: ["image6_futo_cafe.jpg"]
+    images: ["https://i.ibb.co/TDFDvdNM/pm-1754255249623-cmp.jpg"]
   },
   {
     name: "Old SEET Head Building",
     description: "Historic administrative building that formerly housed the School of Engineering and Engineering Technology (SEET) headquarters",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+Old+SEET+Building",
-    images: ["image7_old_seet_head.jpg", "image8_old_seet_head2.jpg"]
+    images: ["https://i.ibb.co/rRd58X58/pm-1754255250073-cmp.jpg", "https://i.ibb.co/h11WcLjY/pm-1754255250526-cmp.jpg"]
   },
   {
     name: "Hostel D (Girls Hostel)",
     description: "Female student accommodation facility with multiple rooms providing on-campus housing for female undergraduates",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+Hostel+D",
-    images: ["image9_hostel_d.jpg"]
+    images: ["https://i.ibb.co/Qjdqpdwd/pm-1754255249027-cmp.jpg"]
   },
   {
     name: "PG Hostel",
     description: "Postgraduate student accommodation facility providing housing for graduate students and researchers",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+PG+Hostel",
-    images: ["image10_pg_hostel.jpg"]
+    images: ["https://i.ibb.co/JWGfq50d/pm-1754255247919-cmp.jpg"]
   },
   {
     name: "Hostel C (Girls Hostel)",
@@ -67,7 +67,7 @@ export const allPlacesInFuto = [
     description: "Male student accommodation funded by the Niger Delta Development Commission, providing modern housing facilities",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+NDDC+Male+Hostel",
-    images: ["image12_nddc_hostel.jpg"]
+    images: ["https://i.ibb.co/CpZPVRQ2/pm-1754255246914-cmp.jpg"]
   },
   {
     name: "Hostel A (Male Hostel)",
@@ -81,14 +81,14 @@ export const allPlacesInFuto = [
     description: "Male student accommodation funded by the Tertiary Education Trust Fund (TETFund), featuring modern facilities and amenities",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+Tetfund+Hostel+B",
-    images: ["image15_tetfund_hostel_b.jpg"]
+    images: ["https://i.ibb.co/tpvbFzD4/pm-1754255246359-cmp.jpg"]
   },
   {
     name: "CESPESS Building (FUTOWA)",
     description: "Centre for Space Science and Engineering Studies building, housing advanced research facilities and laboratories",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+CESPESS+Building",
-    images: ["image16_cespess_futowa.jpg"]
+    images: ["https://i.ibb.co/0Rgwg5dG/pm-1754255244706-cmp.jpg"]
   },
   {
     name: "Public Procurement Building",
@@ -98,108 +98,126 @@ export const allPlacesInFuto = [
     images: ["https://i.ibb.co/tp3wrXtg/pm-1754255244191-cmp.jpg"]
   },
   {
+    name: "Love Garden",
+    description: "A hang out arena built by the SUG for students to hang out with their peers and enjoy freesh air",
+    address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
+    mapLink: "https://maps.google.com/?q=FUTO+Public+Procurement+Building",
+    images: ["https://i.ibb.co/23TS61vj/pm-1754255245721-cmp.jpg"]
+  },
+  {
     name: "School of Information and Communication Technology (SICT) Building",
     description: "Modern academic facility housing the School of Information and Communication Technology with computer labs and lecture halls. Also includes CESPESS facilities",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.app.goo.gl/BqB86a4x4mxLJrzA7",
-    images: ["https://i.ibb.co/HDmJZR9N/48c5eedb22980d87e23ab0661a171807.webp", "https://i.ibb.co/b582LsyT/2034be87362b0a2b1e67c0941246d10f.webp", "https://i.ibb.co/G39bSWVG/unnamed-1.jpg","https://i.ibb.co/3nT8bKL/unnamed.jpg", "https://i.ibb.co/b58M8mP9/unnamed-2.jpg"]
+    images: ["https://i.ibb.co/HDmJZR9N/48c5eedb22980d87e23ab0661a171807.webp", "https://i.ibb.co/b582LsyT/2034be87362b0a2b1e67c0941246d10f.webp", "https://i.ibb.co/mr9yHqj3/48c5eedb22980d87e23ab0661a171807.webp", "https://i.ibb.co/fJPFZBb/2267039237032d7b010b5a26c194b741.webp", "https://i.ibb.co/RGdDKVbv/2034be87362b0a2b1e67c0941246d10f.webp", "https://i.ibb.co/DX7yVFh/b70712eb54b766fce5b6f54749da2a28.webp","https://i.ibb.co/zhbPsBPx/cfc84c89cd9b71049723ad989fba248b.webp","https://i.ibb.co/hFFZSSVQ/62bce6ac7dfc7006f4c6a1ed5915cf24.webp"]
   },
   {
-    name: "Sand Dredging Site",
+    name: "Sand Dredging Site/Otamiri",
     description: "Active sand dredging and extraction site on FUTO campus, used for construction and development projects",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+Sand+Dredging+Site",
-    images: ["https://i.ibb.co/2rdtR6y/23716dd92ed808077b7e8e00c3d155ec.webp"]
+    images: ["https://i.ibb.co/2rdtR6y/23716dd92ed808077b7e8e00c3d155ec.webp", "https://i.ibb.co/8DFKsByY/ef100ce1af86171a1c2fe6cceb0455fe.webp", "https://i.ibb.co/wZVCH8sT/494bcb8f813c7b46ad7cc528a1d11387.webp","https://i.ibb.co/nMV89H64/23716dd92ed808077b7e8e00c3d155ec.webp","https://i.ibb.co/Psz0BM7V/35b6ed41862f94e3128425c669b4dec3.webp", "https://i.ibb.co/hR736QPQ/d7dba24fc54927b63b4e220748cfe62f.webp", "https://i.ibb.co/JR0yybRZ/73803a3735b716c18d703c66e160c32b.webp", "https://i.ibb.co/20Hpxc3R/d2b00512f6cf2fdf27f7f5f7 c9544c2d.webp","https://i.ibb.co/ksTQjybX/af7f8c399e0a31cdd084e64f8522b242.webp", "https://i.ibb.co/hFRw2BNj/36a19c01befd94825d49f810029d63bb.webp", "https://i.ibb.co/KcYKXYvp/5d5a12676ecf4f67f60e0094075a566d.webp", "https://i.ibb.co/9kjrfbTL/d5e01352af93beab9f6d2a53bafe4232.webp", "https://i.ibb.co/RTnHTjMr/069605cb7be998eca4d8108d632fe730.webp",
+"https://i.ibb.co/hJ0Nj8KZ/2004073bcba3a3c6af37b6ad87330e60.webp",
+"https://i.ibb.co/JWbjbPXB/6f3b2d9ed5f2361ef5069cfec0ca2e5c.webp",
+"https://i.ibb.co/7xkqDPQj/b0077802303ab81763559635a9649ce2.webp",
+"https://i.ibb.co/Vccx0s12/15c0bd12e9c73ff08ee18936ae900087.webp",
+"https://i.ibb.co/mQtYCBL/27b35472fc71cff90b415032e9a6565e.webp",
+"https://i.ibb.co/pvk8db0g/566f75ad128916cef0dde864c02d946c.webp",
+"https://i.ibb.co/Py8DN3T/d638821ea49c4d151195f4978a2f3940.webp",
+"https://i.ibb.co/M5nSC8CW/ca1c2070706843c0b3d8d807ba31d713.webp",
+"https://i.ibb.co/XfssyshZ/1d7680d9391a48b602b9b2f403b51e44.webp",
+"https://i.ibb.co/Fbh95NrX/36769ea627a0383beb0822911adb271d.webp",
+"https://i.ibb.co/xKBZ4kjK/f18fb7612c7313919cc19c662d76af77.webp"]
   },
   {
     name: "Nuclear Energy Building",
     description: "Academic building housing nuclear energy research and related programs within the university",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+Nuclear+Energy+Building",
-    images: ["image1_nuclear_energy.jpg"]
+    images: ["https://i.ibb.co/BHTLqypc/pm-1746871741650-cmp.jpg"]
   },
   {
     name: "Physics Department",
     description: "Department building housing the Physics program with lecture halls, laboratories, and research facilities",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+Physics+Department",
-    images: ["image2_physics_dept.jpg", "image10_physics_dept2.jpg"]
+    images: ["https://i.ibb.co/PsnhBFjm/pm-1746871742153-cmp.jpg", "https://i.ibb.co/nMGQHy9j/fc8336712597f7a57526a1f661e97dda.webp"]
   },
   {
     name: "Hall of Mercy",
     description: "Large auditorium and event hall used for ceremonies, lectures, and university functions",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+Hall+of+Mercy",
-    images: ["image3_hall_of_mercy.jpg"]
+    images: ["https://i.ibb.co/h6jsnFw/pm-1746871740724-cmp.jpg"]
   },
   {
     name: "Old SEET Roundabout",
     description: "Major traffic roundabout near the old School of Engineering and Engineering Technology complex, serving as a key campus landmark and junction",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+Old+SEET+Roundabout",
-    images: ["image4_old_seet_roundabout.jpg"]
+    images: ["https://i.ibb.co/4Rk9Xv4V/pm-1746871741214-cmp.jpg"]
   },
   {
     name: "FUTOWA Building",
     description: "FUTO Works and Physical Planning building, administrative facility managing campus infrastructure and development",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+FUTOWA+Building",
-    images: ["image6_futowa.jpg"]
+    images: ["https://i.ibb.co/sp89M620/5d3c352f853a622315e8f1dac14e15c7.webp", "https://i.ibb.co/d0kwp85G/8b46ad2bf1066371b242baafd67c36a0.webp"]
   },
   {
     name: "Architecture Building",
     description: "Academic building housing the Department of Architecture with studios, design labs, and exhibition spaces",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+Architecture+Building",
-    images: ["image7_architecture.jpg"]
+    images: ["https://i.ibb.co/3yGvZZVH/5e0ffc9c28fc6f76a89239babaf83f9f.webp"]
   },
   {
     name: "1000 Capacity Hall",
     description: "Large lecture hall with seating capacity for 1000 students, used for mass lectures and events. Part of the TETFund SIP 2010 project",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+1000+Capacity+Hall",
-    images: ["image8_1000_caps_hall.jpg"]
+    images: ["https://i.ibb.co/1YPrMJS3/02a42c24375d00cc54ce5900943d43b3.webp"]
   },
   {
     name: "SOES Lecture Hall",
     description: "School of Environmental Sciences lecture hall building with classrooms and teaching facilities",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+SOES+Lecture+Hall",
-    images: ["image9_soes_lecture_hall.jpg"]
+    images: ["https://i.ibb.co/M5C802mK/bce164e1a66c76bb66fb9dfd7fb742da.webp"]
   },
   {
     name: "FWT Hall",
     description: "Food and Water Technology lecture hall and laboratory facility for related academic programs",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+FWT+Hall",
-    images: ["image11_fwt_hall.jpg"]
+    images: ["https://i.ibb.co/d0kwp85G/8b46ad2bf1066371b242baafd67c36a0.webp"]
   },
   {
     name: "College of Medicine",
     description: "Medical college complex housing medical education programs, lecture theaters, and clinical training facilities",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+College+of+Medicine",
-    images: ["image12_college_of_medicine.jpg"]
+    images: ["https://i.ibb.co/B27xQk10/03db7301754cb980406b51fadd96692c.webp"]
   },
   {
     name: "Student Affairs Office",
     description: "Administrative building housing student services, student union activities, and student welfare offices",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+Student+Affairs",
-    images: ["image13_student_affairs.jpg"]
+    images: ["https://i.ibb.co/CpT7YgXR/bc0a613860c32bb2c443844613ada3e8.webp"]
   },
   {
     name: "Workshop",
     description: "Engineering workshop facility with machinery and equipment for practical training and fabrication work",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+Workshop",
-    images: ["image14_workshop.jpg"]
+    images: ["https://i.ibb.co/xq7F4PCT/0d480dc6b60b47e6ecbd7ab47b9c10d7.webp"]
   },
   {
     name: "Library and Bookshop",
     description: "Main university library and bookshop complex providing academic resources, study spaces, and educational materials",
     address: "Federal University of Technology Owerri Campus, Owerri, Imo State, Nigeria",
     mapLink: "https://maps.google.com/?q=FUTO+Library",
-    images: ["image15_library_bookshop.jpg"]
+    images: ["https://i.ibb.co/TBjpqhDc/5d6879f815c4f47390c778d0ce1dbcdf.webp"]
   }
 ];
