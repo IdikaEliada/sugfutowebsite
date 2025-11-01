17febf21b259bf25f45590a33c200da9e8f8cc31
IdikaEliada <idika1996@gmail.com>
2025-11-01 21:33:16 +0100

    update

--- data/timetables.js
+++ data/timetables.js
@@ -1,49 +1,55 @@
 export const allTimetables = [
   {
+    faculty: "FUTO",
+    path: "../data/PDFs/2025-2026 SESSION HARMATTAN SEMESTER LECTURE TIMETABLE DRAFT.pdf",
+    description: "The general academics timetable for every level",
+    name: "2025-2026 Session Harmattan Semester Lecture Timetable"
+  },
+  {
     faculty: "SICT",
-    resource: "School of Information and Communication Technology",
+    path: "../data/PDFs/2025-2026 SICT Harmattan Semester Lecture Timetable (Full Draft).pdf",
     description: "2025/2026 Harmattan Semester Lecture Timetable for SICT - includes CSC, CYB, IFT, and SOE departments with various lecture halls and labs",
     name: "2025-2026 SICT Harmattan Semester Lecture Timetable"
   },
   {
     faculty: "SOHT",
-    resource: "School of Health Technology",
+    path: "../data/PDFs/2025 2026 SOHT HARMATTAN LECTURE TIME TABLE - PDF.pdf",
     description: "2025/2026 Harmattan Semester Lecture Timetable for School of Health Technology",
     name: "2025-2026 SOHT Harmattan Lecture Timetable"
   },
   {
     faculty: "SoBS",
-    resource: "School of Biological Sciences",
+    path: "School of Biological Sciences",
     description: "2025/2026 Harmattan Semester Lecture Timetable for School of Biological Sciences",
     name: "2025-2026 SoBS Harmattan Lecture Timetable"
   },
   {
     faculty: "SESET",
-    resource: "School of Engineering and Engineering Technology",
+    path: "School of Engineering and Engineering Technology",
     description: "2025/2026 Harmattan Semester Lecture Timetable for SESET",
     name: "2025-2026 SESET Harmattan Lecture Timetable"
   },
   {
     faculty: "SEET",
-    resource: "School of Engineering and Engineering Technology",
+    path: "../data/PDFs/2025-2026 Harmattan Sem. Lecture Timetable for University and SEET Common Courses.pdf",
     description: "2025-2026 Harmattan Semester Lecture Timetable for University and SEET Common Courses",
     name: "2025-2026 Harmattan Sem. Lecture Timetable for University and SEET Common Courses"
   },
   {
     faculty: "SOBS",
-    resource: "School of Biological Sciences",
+    path: "School of Biological Sciences",
     description: "2025/2026 Harmattan Semester Lecture Timetable for SOBS",
     name: "2025-2026 SOBS Harmattan Lecture Timetable"
   },
   {
     faculty: "SLIT",
-    resource: "School of Logistics and Innovation Technology",
+    path: "School of Logistics and Innovation Technology",
     description: "2025/2026 Harmattan Semester Undergraduate Lecture Timetable for SLIT",
     name: "Draft 1 HARMATTAN SLIT Undergrad Lecture Timetable"
   },
   {
     faculty: "SOES",
-    resource: "School of Environmental Sciences",
+    path: "../data/PDFs/2025-2026 SOES HARMATTAN LECTURE TIME TABLE.pdf",
     description: "2025/2026 Harmattan Semester Lecture Timetable for SOES",
     name: "2025-2026 SOES Harmattan Lecture Timetable"
   }
