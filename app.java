133dbe4e25b59bf24b2e947c02bf8bac52fc5a7a
idikaEliada <idika1996@gmail.com>
2026-01-31 17:19:46 +0100

    update

---
+++ app.java
@@ -0,0 +1,12 @@
+"redirects": [
+  {
+    "source": "/resources/:path*.html",
+    "destination": "/resources/:path*",
+    "permanent": true
+  },
+  {
+    "source": "/:path*.html",
+    "destination": "/:path*",
+    "permanent": true
+  }
+],
