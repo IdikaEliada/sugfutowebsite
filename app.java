4b9ace034f479cead14d632422ca326935f6d53e
idikaEliada <idika1996@gmail.com>
2026-02-01 10:40:10 +0100

    fixing tailwind theme

--- app.java
+++ app.java
@@ -1,12 +1,3 @@
-"redirects": [
-  {
-    "source": "/resources/:path*.html",
-    "destination": "/resources/:path*",
-    "permanent": true
-  },
-  {
-    "source": "/:path*.html",
-    "destination": "/:path*",
-    "permanent": true
-  }
-],
+public static void main (String[] args) {
+  
+}
