aac83aa62fda91f26f837dc6ca7b3f93f8c74b6c
idikaEliada <idika1996@gmail.com>
2026-03-09 11:12:57 +0100

    update new.cpp

--- new.cpp
+++ new.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+
+int add(int a, int b){
+  return a + b;
+}
+
 int main () {
-  std::cout << "Hello" << std::endl;
+  int x = add (2, 3)
+  
+  std::cout << "Hello"  << std::endl;
+  
+  std::cout << x << std::endl;
 }
