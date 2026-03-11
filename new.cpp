a36d79b9922f002e99749626c9a4ba49050dfebe
idikaEliada <idika1996@gmail.com>
2026-03-11 16:19:58 +0100

    update new.cpp

--- new.cpp
+++ new.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <studio.h>
 
 
 int add(int a, int b){
   return a + b;
 }
+
 
 int main () {
   int x = add (2, 3)
@@ -11,4 +13,5 @@ int main () {
   std::cout << "Hello"  << std::endl;
   
   std::cout << x << std::endl;
+  
 }
